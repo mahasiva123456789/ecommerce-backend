5cb0a0e62f86f63747e85271fafc1f13ecff568c
mahasiva123456789 <mahasiva2606@gmail.com>
2024-08-13 21:09:43 +0530

    converting the post to get

--- routes/Routes.js
+++ routes/Routes.js
@@ -14,7 +14,7 @@ router.put('/products/:id',productController.updateProducts)
 
 // user routers
 router.post('/users',userController.createuser)
-router.post('/users',userController.login)
+router.get('/users',userController.login)
 
 //cart routers
 router.post('/carts/createcart',auth,cartcontroller.createCart)
